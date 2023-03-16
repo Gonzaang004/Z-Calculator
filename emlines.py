23d90138bb658e23078854526e2d56f2c7b0e29d
Angel Gonzalez <gonzaang@uw.edu>
2023-03-16 21:47:18 +0000

    Adding new files to repository

---
+++ emlines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+emlines = ["Ly-alpha 1216A (usually broad)",
+"NV 1240A (usually broad, often blended with Lya)",
+"SiIV/OIV 1400A (usually broad)",
+"CIV 1549A (usually broad)",
+"CIII 1909A (usually broad)",
+"MgII 2800A (usually broad)",
+"OII 3727A (usually narrow)",
+"H-gamma 4340A ( usually broad, but weak)",
+"H-beta 4861A (usually broad)",
+"OIII 4959/5007 (usually narrow)",
+"H-alpha 6563 (usually broad)",]
+
+print(emlines)
+
+
